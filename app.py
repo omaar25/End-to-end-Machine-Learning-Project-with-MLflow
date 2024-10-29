27a93e808172c3851e9972ce575b65ad6ff999fe
omaar25 <mohamedomar.benmakhlouf@etudiant-enit.utm.tn>
2024-10-29 23:42:18 +0100

    app added

--- app.py
+++ app.py
@@ -11,10 +11,38 @@ app = Flask(__name__)
 def homePage():
     return render_template("index.html")
 
-@app.route('/train',methods=['GET'])  # route to train the pipeline
+@app.route('/train',methods=['GET'])  
 def training():
     os.system("python main.py")
     return "Training Successful!" 
+
+@app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
+def index():
+    if request.method == 'POST':
+        try:
+            #  reading the inputs given by the user
+            type =float(request.form['Type'])
+            Air_temperature_C =float(request.form['Air_temperature_C'])
+            Process_temperature_C =float(request.form['Process_temperature_C'])
+            Rotational_Speed_RPM =float(request.form['Rotational_Speed_RPM'])
+            Torque_Nm =float(request.form['Torque_Nm'])
+            Tool_Wear_min =float(request.form['Tool_Wear_min'])
+         
+            data = [type,Air_temperature_C,Process_temperature_C,Rotational_Speed_RPM,Torque_Nm,Tool_Wear_min]
+            data = np.array(data).reshape(1, 11)
+            
+            obj = PredictionPipeline()
+            predict = obj.predict(data)
+
+            return render_template('index.html', prediction = str(predict))
+
+        except Exception as e:
+            print('The Exception message is: ',e)
+            return 'something is wrong'
+
+    else:
+        return render_template('index.html')
+
 
 
 if __name__ == "__main__":
