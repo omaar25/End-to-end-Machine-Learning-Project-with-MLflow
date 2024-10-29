65bdba3166bc9f5cf82d5e95fb8cfc2d304d12cd
omaar25 <mohamedomar.benmakhlouf@etudiant-enit.utm.tn>
2024-10-29 23:42:06 +0100

    app added

--- app.py
+++ app.py
@@ -11,9 +11,13 @@ app = Flask(__name__)
 def homePage():
     return render_template("index.html")
 
-
+@app.route('/train',methods=['GET'])  # route to train the pipeline
+def training():
+    os.system("python main.py")
+    return "Training Successful!" 
 
 
 if __name__ == "__main__":
 	# app.run(host="0.0.0.0", port = 8080, debug=True)
 	app.run(host="0.0.0.0", port = 8080)
+      
