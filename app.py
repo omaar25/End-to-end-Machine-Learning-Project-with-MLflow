0722e80e1a51ff71628ac841b511a9ae1c884fc6
omaar25 <mohamedomar.benmakhlouf@etudiant-enit.utm.tn>
2024-10-30 16:49:42 +0100

    app added

--- app.py
+++ app.py
@@ -1,83 +1,27 @@
 from flask import Flask, render_template, request
-import os 
-import numpy as np
-from src.ML.pipeline.prediction import PredictionPipeline
-from src.ML.components.data_processing import DataProcessing
-from src.ML.config.configuration import ConfigurationManager
-import pandas as pd
-import pickle
+from ML.pipeline.AppPipeline import AppPipelinePrediction
+
+
+
+pipeline = AppPipelinePrediction()
 app = Flask(__name__)
 
-# Configuration setup
-config = ConfigurationManager().get_data_processing_config()
-processor = DataProcessing(config)
 
 @app.route('/', methods=['GET'])  
 def homePage():
     return render_template("index.html")
 
 
-#@app.route('/train', methods=['GET'])  
-
-#def training():
- #   try:
-  #      os.system("python main.py")  # This should trigger your training script
-   #     return "Training Successful!" 
-    #except Exception as e:
-     #   return f"An error occurred while training: {str(e)}"
-
 @app.route('/predict', methods=['POST'])
-def index():
+def predict():
     if request.method == 'POST':
         try:
-
-            Type = request.form['Type']
-            Air_temperature_K = float(request.form['Air_temperature_K'])
-            Process_temperature_K = float(request.form['Process_temperature_K'])
-            Rotational_Speed_RPM = float(request.form['Rotational_Speed_RPM'])
-            Torque_Nm = float(request.form['Torque_Nm'])
-            Tool_Wear_min = float(request.form['Tool_Wear_min'])
-            data = np.array([Type, Air_temperature_K, Process_temperature_K, 
-                             Rotational_Speed_RPM, Torque_Nm, Tool_Wear_min]).reshape(1,-1)
-            
-            columns = ['Type','Air temperature [K]','Process temperature [K]','Rotational speed [rpm]',
-                       'Torque [Nm]','Tool wear [min]',
-                       ]
-            
-            input_df = pd.DataFrame(data, columns=columns)
-
-
-            input_df['Air temperature [K]'] = input_df['Air temperature [K]'].astype(float)
-            input_df['Process temperature [K]'] = input_df['Air temperature [K]'].astype(float)
-            input_df['Rotational speed [rpm]'] = input_df['Rotational speed [rpm]'].astype(float)
-            input_df['Torque [Nm]'] = input_df['Torque [Nm]'].astype(float)
-            input_df['Tool wear [min]'] = input_df['Tool wear [min]'].astype(float)
-
-            processor.df = input_df
-            processor.rename_columns()
-            processor.convert_temperature()
-            scaler_path = os.path.join(config.root_dir, "scaler.pkl")
-            with open(scaler_path, 'rb') as f:
-                scaler = pickle.load(f)
-            col_to_scale = ['Rotational_Speed_RPM', 'Torque_Nm', 'Tool_Wear_min', 'Air_temperature_C', 'Process_temperature_C']
-            processor.df[col_to_scale] = scaler.transform(processor.df[col_to_scale])
-            processor.encode_features()
-            
-            obj = PredictionPipeline()
-            encoded_predict = obj.predict(processor.df.values)
-            
-            failure_type = processor.decode_failure_type(encoded_predict[0])
-            
-            return render_template('index.html', prediction=failure_type)
-
-        except ValueError as ve:
-            return f'Input error: {str(ve)}. Please check your inputs.'
+            prediction = pipeline.predict(request.form)
+            return render_template('index.html', prediction=prediction)
         except Exception as e:
-            print('Exception:', e)
-            return 'Something went wrong during prediction.'
+            raise e
 
     return render_template('index.html')
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/ML/pipeline/AppPipeline.py
@@ -0,0 +1,74 @@
+import os
+from pathlib import Path
+import joblib
+import numpy as np
+import pandas as pd
+from ML.config.configuration import ConfigurationManager
+from src.ML.components.data_processing import DataProcessing
+
+class AppPipelinePrediction:
+    def __init__(self):
+        self.config = ConfigurationManager()
+        self.scaler = self.load_scaler()
+        self.best_model = self.load_model()
+
+    def load_scaler(self):
+        scaler_path = os.path.join(self.config.get_data_processing_config().root_dir, "scaler.pkl")
+        try:
+            with open(scaler_path, 'rb') as f:
+                scaler = joblib.load(f)
+            return scaler
+        except FileNotFoundError:
+            raise FileNotFoundError(f"No scaler.pkl file found at {self.scaler_path}")
+
+    def load_model(self):
+        model_path = Path(self.config.get_model_evaluation_config().root_dir)
+        try:
+            model_file = next(model_path.glob("*.joblib"))
+            model = joblib.load(model_file)
+            return model
+        except StopIteration:
+            raise FileNotFoundError(f"No .joblib model file found in {self.model_path}")
+
+    def preprocess_input(self, form_data):
+        Type = form_data['Type']
+        Air_temperature_K = float(form_data['Air_temperature_K'])
+        Process_temperature_K = float(form_data['Process_temperature_K'])
+        Rotational_Speed_RPM = float(form_data['Rotational_Speed_RPM'])
+        Torque_Nm = float(form_data['Torque_Nm'])
+        Tool_Wear_min = float(form_data['Tool_Wear_min'])
+
+        # Create DataFrame
+        data = np.array([Type, Air_temperature_K, Process_temperature_K, 
+                         Rotational_Speed_RPM, Torque_Nm, Tool_Wear_min]).reshape(1, -1)
+        columns = ['Type', 'Air temperature [K]', 'Process temperature [K]', 'Rotational speed [rpm]',
+                   'Torque [Nm]', 'Tool wear [min]']
+        df = pd.DataFrame(data, columns=columns)
+
+        df['Air temperature [K]'] = df['Air temperature [K]'].astype(float)
+        df['Process temperature [K]'] = df['Air temperature [K]'].astype(float)
+        df['Rotational speed [rpm]'] = df['Rotational speed [rpm]'].astype(float)
+        df['Torque [Nm]'] = df['Torque [Nm]'].astype(float)
+        df['Tool wear [min]'] = df['Tool wear [min]'].astype(float)
+
+        # Attach DataFrame to DataProcessing instance and process
+        data_processor = DataProcessing(self.config.get_data_processing_config())
+        data_processor.df = df
+        data_processor.rename_columns()
+        data_processor.convert_temperature()
+
+        # Scale required columns
+        col_to_scale = ['Rotational_Speed_RPM', 'Torque_Nm', 'Tool_Wear_min', 'Air_temperature_C', 'Process_temperature_C']
+        data_processor.df[col_to_scale] = self.scaler.transform(data_processor.df[col_to_scale])
+
+        # Encode categorical features if needed
+        data_processor.encode_features()
+
+        # Return the processed DataFrame
+        return data_processor.df
+
+    def predict(self, form_data):
+        data_processor = DataProcessing(self.config.get_data_processing_config())
+        processed_df = self.preprocess_input(form_data)
+        prediction = self.best_model.predict(processed_df.values)
+        return data_processor.decode_failure_type(prediction[0])
